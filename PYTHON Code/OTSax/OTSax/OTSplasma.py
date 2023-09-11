Fixed the gradient computation

PYTHON Code/OTSax/OTSax/OTSplasma.py
@@ -2,7 +2,7 @@ import jax.numpy as jnp
 import jax
 from .OTSconstants import *
 from .tools.Faddeeva import *
-from .tools.CauchyPV import KellerWrobel_PV
+from .tools.CauchyPV import *
 
 #%%########################################################################
 #    plasma response integral [p106 Froula2011]
@@ -30,7 +30,10 @@ def plaZim(z):
 
 # Custom plasma dispersion function
 @jax.jit
-def custZprime(v, dfdv, f_params, maxv, integration_scale = 1.1):  
+def custZprime(v, dists, maxv, integration_scale = 1.1):  
+
+    dfdv = dists['dfi/dv']
+    f_params = dists['fi_params']
 
     # Adjust integration range
     v_scale = integration_scale*maxv
@@ -38,12 +41,12 @@ def custZprime(v, dfdv, f_params, maxv, integration_scale = 1.1):
 
     # Jitted by lax.scan
     def PV_calc(sum,tau):
-        y = KellerWrobel_PV(tau, dfdv, f_params, v_scale)
+        y = CutOutSingularity_w_Limit_PV(tau, dfdv, f_params, v_scale)
         sum += y
         return sum, y
     
     _,dWr = jax.lax.scan(PV_calc,jnp.array([0.0]),x)
-    dWi = -jnp.pi*dfdv(v,*f_params)
+    dWi = -jnp.pi*dists['dfi/dv_vmap'](v,*f_params)
 
     # Safety
     dWr = jnp.where(jnp.isnan(dWr), 0.0, dWr)
@@ -117,10 +120,10 @@ def chicust_i(kw_dict,dists):
     vel   = kw_dict['vel']/c
     vemax = jnp.max(jnp.abs(vel))
     ##################### numerical
-    Zpe = custZprime(vel, dists['dfi/dv'], dists['fi_params'], vemax)   
+    Zpi = custZprime(vel, dists, vemax)   
 
-    chiEre= (-(1.0/(c*k[:]/omgpi)**2))*(Zpe[0,:])  #me/mi
-    chiEim= (-(1.0/(c*k[:]/omgpi)**2))*(-1j*Zpe[1,:])  #me/mi 
+    chiEre= (-(1.0/(c*k[:]/omgpi)**2))*(Zpi[0,:])  #me/mi
+    chiEim= (-(1.0/(c*k[:]/omgpi)**2))*(-1j*Zpi[1,:])  #me/mi 
     return chiEre+chiEim
 
 #chi e
@@ -131,7 +134,7 @@ def chicust_e(kw_dict,dists):
     vel   = kw_dict['vel']/c
     vemax = jnp.max(jnp.abs(vel))
     ##################### numerical
-    Zpe = custZprime(vel, dists['dfe/dv'], dists['fe_params'], vemax)  
+    Zpe = custZprime(vel, dists, vemax)  
     
     chiEre= (-(1.0/(c*k[:]/omgpe)**2))*(Zpe[0,:]) 
     chiEim= (-(1.0/(c*k[:]/omgpe)**2))*(-1j*Zpe[1,:]) 

PYTHON Code/OTSax/OTSax/OTSutils.py
@@ -6,12 +6,13 @@ from jax.tree_util import Partial
 def d_distfunc_dv(dist_func,Nparams):
     in_axes_tuple = (0,) + (None,)*Nparams
     graddfdv = jax.vmap(jax.grad(dist_func),in_axes=in_axes_tuple,out_axes=0)
-    return jax.jit(lambda v,*_params : graddfdv(jnp.atleast_1d(v),*_params))
+    # return jax.jit(lambda v,*_params : graddfdv(jnp.atleast_1d(v),*_params))
+    return jax.jit(graddfdv)
 
 def create_dist_func_dict(dist_func,*_params):
     Nparams = len(_params)
     graddfdv = d_distfunc_dv(dist_func,Nparams)
-    dists = {'fi' : Partial(dist_func), 'dfi/dv' : Partial(graddfdv), 'fi_params' : [*_params]}
+    dists = {'fi' : Partial(dist_func), 'dfi/dv' : Partial(jax.jit(jax.grad(dist_func))), 'dfi/dv_vmap' : Partial(graddfdv), 'fi_params' : [*_params]}
     return dists
 
 def get_kw_vals(omg,omgL,sa,Z,Ai,ne):

PYTHON Code/OTSax/OTSax/tools/CauchyPV.py
@@ -2,7 +2,10 @@ import jax
 import jax.numpy as jnp
 from .adaptive_quad import quad
 
-def KellerWrobel_PV(tau, dfdv, f_params, v_scale):
+epsilon_tau = 1e-10
+
+@jax.jit
+def CutOutSingularity_w_Limit_PV(tau, dfdv, f_params, v_scale):
     """
     
     Calculates Cauchy princpal value of:
@@ -11,18 +14,96 @@ def KellerWrobel_PV(tau, dfdv, f_params, v_scale):
 
     See https://www.sciencedirect.com/science/article/pii/S0377042715004422?via%3Dihub
     
+    Equation 2.4, "subtracting out the singularity"
+
     """
 
+    @jax.jit
+    def f(x,v_scale,*_parameters):
+        v = x*v_scale
+        return jnp.reshape(dfdv(v,*_parameters), ())
+
+    dfdx = jax.grad(f,argnums=0)
+
+    @jax.jit
+    def g_integrand(x,tau,v_scale,*_parameters):
+        fx = f(x,v_scale,*_parameters)
+        ftau = f(tau,v_scale,*_parameters)
+        limit = dfdx(tau,v_scale,*_parameters)
+        g = jnp.where(jnp.abs(x-tau) > epsilon_tau, (fx-ftau)/(x-tau), limit)
+        return jnp.reshape(g, ())
+
+    I_tau = f(tau,v_scale,*f_params)*jnp.log((1.0-tau)/(1.0+tau))
+
+    integrand_params = [tau,v_scale,*f_params]
+
+    I_g = quad(g_integrand,-1.0,1.0,integrand_params)
+
+    return I_tau+I_g
+
+@jax.jit
+def CutOutSingularity_PV(tau, dfdv, f_params, v_scale):
+    """
+    
+    Calculates Cauchy princpal value of:
+
+    \int_{-1}^{+1} f(x)/(x-tau) dx
+
+    See https://www.sciencedirect.com/science/article/pii/S0377042715004422?via%3Dihub
+    
+    Equation 2.4, "subtracting out the singularity"
+
+    """
+
+    @jax.jit
     def f(x,v_scale,*_parameters):
         v = x*v_scale
         return dfdv(v,*_parameters)
 
+    @jax.jit
     def g_integrand(x,tau,v_scale,*_parameters):
         fx = f(x,v_scale,*_parameters)
         ftau = f(tau,v_scale,*_parameters)
         g = (fx-ftau)/(x-tau)
         return jnp.reshape(g, ())
 
+    I_tau = f(tau,v_scale,*f_params)*jnp.log((1.0-tau)/(1.0+tau))
+
+    integrand_params = [tau,v_scale,*f_params]
+
+    I_gp = quad(g_integrand,tau,1.0,integrand_params)
+    I_gm = quad(g_integrand,-1.0,tau,integrand_params)
+    I_g = I_gp+I_gm
+
+    return I_tau+I_g
+
+@jax.jit
+def KellerWrobel_PV(tau, dfdv, f_params, v_scale):
+    """
+    
+    Calculates Cauchy princpal value of:
+
+    \int_{-1}^{+1} f(x)/(x-tau) dx
+
+    See https://www.sciencedirect.com/science/article/pii/S0377042715004422?via%3Dihub
+
+    Equation 2.5
+    
+    """
+
+    @jax.jit
+    def f(x,v_scale,*_parameters):
+        v = x*v_scale
+        return dfdv(v,*_parameters)
+
+    @jax.jit
+    def g_integrand(x,tau,v_scale,*_parameters):
+        fx = f(x,v_scale,*_parameters)
+        ftau = f(tau,v_scale,*_parameters)
+        g = (fx-ftau)/(x-tau)
+        return jnp.reshape(g, ())
+
+    @jax.jit
     def h_integrand(x,tau,v_scale,*_parameters):
         ftaup = f(tau+x,v_scale,*_parameters)
         ftaum = f(tau-x,v_scale,*_parameters)